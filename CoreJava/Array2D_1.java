a97aaab906310e8d1b7c4c6298e516b5cc29641d
snjava <scnaikwadi2@gmail.com>
2022-05-19 08:55:10 +0530

    Added New file and fixed existing file

--- CoreJava/Array2D_1.java
+++ CoreJava/Array2D_1.java
@@ -8,6 +8,8 @@ public class Array2D_1
 
 		percent[0][1] = 98;
 		percent[1][2] = 72.2;
+		percent[1][1] = 34.3;
+		percent[2][1] = 55.2;
 
 		for ( int r = 0 ; r<3 ; r++ )
 		{
---
+++ CoreJava/ForLoop1.java
@@ -0,0 +1,8 @@
+public class ForLoop1 {
+	public static void main(String ar[]) {
+		for( ;  ; ) 
+		{
+			System.out.println("Good Morning..");
+		}
+	}
+}
